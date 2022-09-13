bb1c36c3680dc83950da6ff804069106b84dff3e
Aman Kumar <amanmandal644@gmail.com>
2022-09-13 10:30:20 +0530

    This is my Day 1 Assignment

---
+++ Day_1.js
@@ -0,0 +1,92 @@
+// ASSIGNMENT I  
+
+// Problem 1:
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+    // **CODE** // 
+
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+
+// Problem 2:
+
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+     
+    // **CODE** // 
+
+let Name2 = "Aman Kumar";
+console.log("Name : ", Name2);
+
+let father_name = "Ajay Prasad Mandal";
+console.log("Father name : ", father_name);
+
+let mother_name = "Minu Devi";
+console.log("Mother name :",mother_name);
+
+
+// Problem 3 :
+
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+    // **CODE** //
+
+
+let Name3 = "Aman Kumar";
+console.log("Name :", Name3);
+console.log("Type of Name is :",typeof(Name3));
+    
+let Age = 24;
+console.log("Age :",Age);
+console.log("Type of Age is :",typeof(Age));
+
+
+
+// Problem 4 :
+
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects.
+// Print the report card of the student (You can make it look nice by using some keyboard symbols ).
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration.
+
+    
+    // **CODE** //
+
+// Student Name 
+let name5 = "闩爪闩𝓝 长ㄩ爪闩尺";
+console.log("Name :",name5);
+
+// Student School Name
+let school = "Ɗ.𝓐.Ʋ Ƥ𝓤ẞⳐｴC SCԊOOⳐ";
+console.log("School :",school);
+
+// Student Grade
+let grade = "XII";
+console.log("Grade : ",grade);
+
+// Student Section 
+let section = "𝓐";
+console.log("Section :",section);
+
+// Student Roll No.
+let roll_no = 18;
+console.log("Roll No. :", roll_no);
+
+// Student Marks in Subject 1 (Maths)
+let subject1 = 88;
+console.log("Mathematics :", subject1);
+
+// Student Marks in Subject 2 (Science)
+let subject2 = 89;
+console.log("Science : ", subject2)
+
+// Student Marks in Subject 3 (Computers)
+let subject3 = 100;
+console.log("Computer :",subject3);
